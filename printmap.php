15818bcad578e042f288ab87ba90e71ba0be9509
U-KCPT19\jglynn <jglynn@DEV01.KCPT19.local>
2016-08-27 16:54:27 -0500

    Print map changes

--- printmap.php
+++ printmap.php
@@ -1,6 +1,5 @@
 <style>
- body {
- 	margin: 0;
- }
+ body { margin: 0; }
+ body::-webkit-scrollbar { width: 0 !important }
 </style>
-<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1fF00y72Z5QU87aii6Hx84KLgMNc" width="300%" height="100%" frameborder="none"></iframe>
+<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1fF00y72Z5QU87aii6Hx84KLgMNc" width="6500px" height="7000px" frameborder="none"></iframe>
