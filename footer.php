cabb79046b6f1a861fbf46b1d1437bdeb2204f6b
Jirrod Glynn <Jirrod Glynn>
2016-11-04 05:48:42 +0100

    Deleted images and rearranged some things

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-	<!-- <script src="./scripts/dropzone.js"></script> -->
-	<script src="./scripts/script.js"></script>
+	<!-- <script src="./js/dropzone.js"></script> -->
+	<script src="./js/script.js"></script>
 
 </body></html>
